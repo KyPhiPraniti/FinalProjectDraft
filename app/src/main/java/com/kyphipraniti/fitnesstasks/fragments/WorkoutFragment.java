9ed76b7869fdf43271a12a8273ad691c9b176fe3
pranitishettiwar <praniti@lumoslabs.com>
2017-10-13 23:33:32 -0700

    Added CardView and RecyclerView for workouts

--- app/src/main/java/com/kyphipraniti/fitnesstasks/fragments/WorkoutFragment.java
+++ app/src/main/java/com/kyphipraniti/fitnesstasks/fragments/WorkoutFragment.java
@@ -1,14 +1,27 @@
 package com.kyphipraniti.fitnesstasks.fragments;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.kyphipraniti.fitnesstasks.R;
+import com.kyphipraniti.fitnesstasks.adapters.RVAdapter;
 
 public class WorkoutFragment extends Fragment {
+
+    //TODO: Change according to Task Model
+    ArrayList workoutNames = new ArrayList<>(Arrays.asList("Core Workout", "Chest Workout", "Leg Workout", "Full Body Workout"));
+    ArrayList workoutImages = new ArrayList<>(Arrays.asList(R.drawable.core, R.drawable.chest_workout, R.drawable.leg_workout, R
+        .drawable.full_body_workout));
+
 
     private OnFragmentInteractionListener mListener;
 
@@ -32,6 +45,20 @@ public class WorkoutFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_workout, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        RecyclerView rv = (RecyclerView) view.findViewById(R.id.rv);
+        rv.setHasFixedSize(true);
+
+        LinearLayoutManager llm = new LinearLayoutManager(getContext());
+        rv.setLayoutManager(llm);
+
+        RVAdapter adapter = new RVAdapter(getContext(), workoutNames, workoutImages);
+        rv.setAdapter(adapter);
     }
 
     @Override
