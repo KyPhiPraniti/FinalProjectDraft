733c575a86e041ae6c4ec691333713ad5b714a9c
pranitishettiwar <praniti@lumoslabs.com>
2017-10-20 22:02:48 -0700

    setup firebase cloud msg for Push Notif

---
+++ app/src/main/java/com/kyphipraniti/fitnesstasks/MyFirebaseInstanceIDService.java
@@ -0,0 +1,18 @@
+package com.kyphipraniti.fitnesstasks;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "MyFirebaseInsIDService";
+
+    @Override
+    public void onTokenRefresh() {
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "New Token: " + refreshedToken);
+
+    }
+}
