d27b861c174b45bb6ad683f5e24cd1717fcf8aa5
Ky Statham <kystatham@gmail.com>
2017-10-22 18:33:29 -0700

    get photo download urls from database

--- app/src/main/java/com/kyphipraniti/fitnesstasks/adapters/ProgressPhotoAdapter.java
+++ app/src/main/java/com/kyphipraniti/fitnesstasks/adapters/ProgressPhotoAdapter.java
@@ -1,6 +1,7 @@
 package com.kyphipraniti.fitnesstasks.adapters;
 
 import android.content.Context;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,8 +9,6 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
-import com.firebase.ui.storage.images.FirebaseImageLoader;
-import com.google.firebase.storage.StorageReference;
 import com.kyphipraniti.fitnesstasks.R;
 
 import java.util.List;
@@ -20,10 +19,10 @@ import java.util.List;
 
 public class ProgressPhotoAdapter extends RecyclerView.Adapter<ProgressPhotoAdapter.ViewHolder> {
 
-    final private List<StorageReference> mProgressPhotos;
+    final private List<Uri> mProgressPhotos;
     private Context mContext;
 
-    public ProgressPhotoAdapter(List<StorageReference> photos) {
+    public ProgressPhotoAdapter(List<Uri> photos) {
         mProgressPhotos = photos;
     }
 
@@ -38,11 +37,9 @@ public class ProgressPhotoAdapter extends RecyclerView.Adapter<ProgressPhotoAdap
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        StorageReference photo = mProgressPhotos.get(position);
-        Glide.with(mContext)
-                .using(new FirebaseImageLoader())
-                .load(photo)
-                .into(holder.ivPhoto);
+        Uri photo = mProgressPhotos.get(position);
+
+        Glide.with(mContext).load(photo).into(holder.ivPhoto);
     }
 
     @Override
--- app/src/main/java/com/kyphipraniti/fitnesstasks/fragments/SummaryFragment.java
+++ app/src/main/java/com/kyphipraniti/fitnesstasks/fragments/SummaryFragment.java
@@ -1,5 +1,6 @@
 package com.kyphipraniti.fitnesstasks.fragments;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,26 +10,31 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
 import com.kyphipraniti.fitnesstasks.R;
 import com.kyphipraniti.fitnesstasks.adapters.ProgressPhotoAdapter;
 import com.kyphipraniti.fitnesstasks.adapters.TasksAdapter;
 import com.kyphipraniti.fitnesstasks.model.Task;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 public class SummaryFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
     private final FirebaseStorage storage = FirebaseStorage.getInstance();
+    private final FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
+    private final DatabaseReference dbReference = mDatabase.getReference();
 
     ArrayList<Task> mTasks;
     RecyclerView rvTasks;
 
-    ArrayList<StorageReference> mProgressPhotos;
+    ArrayList<Uri> mProgressPhotos;
     RecyclerView rvProgressPhotos;
+    ProgressPhotoAdapter progressPhotoAdapter;
 
     public SummaryFragment() {
     }
@@ -58,14 +64,21 @@ public class SummaryFragment extends Fragment {
         rvTasks.setAdapter(adapter);
 
         rvProgressPhotos = v.findViewById(R.id.rvProgressPhotos);
-        //get storage refs from database
         mProgressPhotos = new ArrayList<>();
-        ProgressPhotoAdapter photoAdapter = new ProgressPhotoAdapter(mProgressPhotos);
-        rvProgressPhotos.setAdapter(photoAdapter);
+        ArrayList<String> photoStrings = new ArrayList<String>(Arrays.asList("https://firebasestorage.googleapis.com/v0/b/fitnesstasks.appspot.com/o/stock-photo-portrait-of-young-fitness-woman-flexing-muscles-and-smiling-african-female-model-in-sportswear-564988303.jpg?alt=media&token=7393d5e9-98af-436d-b057-13256596c2b7",
+                "https://firebasestorage.googleapis.com/v0/b/fitnesstasks.appspot.com/o/strong-man-flex-arm-close-very-flexing-his-muscles-32938566.jpg?alt=media&token=c9d24b23-dc0f-4166-8c4c-14815c873a7c",
+                "https://firebasestorage.googleapis.com/v0/b/fitnesstasks.appspot.com/o/images%2Fphoto.jpg?alt=media&token=2a586991-7184-4d33-aebb-80c17c7e5eb1"));
+        for(int i = 0; i < photoStrings.size(); i++) {
+            Uri uri = Uri.parse(photoStrings.get(i));
+            mProgressPhotos.add(uri);
+        }
+        progressPhotoAdapter = new ProgressPhotoAdapter(mProgressPhotos);
+        rvProgressPhotos.setAdapter(progressPhotoAdapter);
 
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
-        rvTasks.setLayoutManager(linearLayoutManager);
-        rvProgressPhotos.setLayoutManager(linearLayoutManager);
+        LinearLayoutManager taskLinearLayoutManager = new LinearLayoutManager(getContext());
+        rvTasks.setLayoutManager(taskLinearLayoutManager);
+        LinearLayoutManager photoLinearLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+        rvProgressPhotos.setLayoutManager(photoLinearLayoutManager);
 
         return v;
     }
@@ -73,6 +86,35 @@ public class SummaryFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+//        final Query progressPhotos = dbReference.child(Constants.FIREBASE_CHILD_USERS).child(Constants.FIREBASE_CHILD_PHOTOS);
+//        progressPhotos.addChildEventListener(new ChildEventListener() {
+//            @Override
+//            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+//                mProgressPhotos.add(dataSnapshot.getValue(Uri.class));
+//                progressPhotoAdapter.notifyItemInserted(mProgressPhotos.size());
+//            }
+//
+//            @Override
+//            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+//
+//            }
+//
+//            @Override
+//            public void onChildRemoved(DataSnapshot dataSnapshot) {
+//
+//            }
+//
+//            @Override
+//            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+//
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//
+//            }
+//        });
     }
 
     @Override
